a54a0398be451dab870f4ba8af6cb6ae4b6731ec
Justin Cremer <jacremer@live.com>
2022-01-18 20:53:50 -0800

    move inner method into generic executor method

--- src/application.rs
+++ src/application.rs
@@ -1,9 +1,6 @@
 use crate::error::InitializationError;
-use bgfx_rs::static_lib::{
-    ClearFlags, DbgTextClearArgs, DebugFlags, Init, PlatformData, RendererType, ResetArgs,
-    ResetFlags, SetViewClearArgs,
-};
-use glfw::{Action, Context, Glfw, Key, Window, WindowEvent, WindowMode};
+use bgfx_rs::static_lib::{DebugFlags, Init, PlatformData, RendererType, ResetFlags};
+use glfw::{Action, Glfw, Key, Window, WindowEvent, WindowMode};
 use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
 use std::sync::mpsc::Receiver;
 
@@ -36,20 +33,20 @@ impl<'a> WindowMetadata<'a> {
 }
 
 /// Wrapper around a glfw window and EventStream for providing initialization abstractions
-pub struct WindowHandle {
+pub struct Application {
     glfw: Glfw,
-    window: glfw::Window,
-    size: (i32, i32),
     event_stream: EventStream,
-    debug_flags: DebugFlags,
+    pub window: glfw::Window,
+    pub size: (u32, u32),
+    pub debug_flags: DebugFlags,
 }
 
-impl WindowHandle {
+impl Application {
     fn new(
         glfw: Glfw,
-        window: Window,
-        size: (i32, i32),
         event_stream: EventStream,
+        window: Window,
+        size: (u32, u32),
         debug_flags: DebugFlags,
     ) -> Self {
         Self {
@@ -75,9 +72,9 @@ impl WindowHandle {
         ) {
             Some((window, event_stream)) => Ok(Self::new(
                 glfw,
-                window,
-                (metadata.width as i32, metadata.height as i32),
                 event_stream,
+                window,
+                (metadata.width, metadata.height),
                 metadata.debug_flags,
             )),
             None => Err(InitializationError::Window),
@@ -87,70 +84,28 @@ impl WindowHandle {
     pub fn init(&mut self) -> Result<(), InitializationError> {
         let mut init = Init::new();
         init.type_r = self.get_render_type();
-        init.resolution.height = 0;
-        init.resolution.width = 0;
-        init.resolution.reset = ResetFlags::VSYNC.bits(); // makes window recreation smooth
+        init.resolution.height = self.size.0;
+        init.resolution.width = self.size.1;
+        init.resolution.reset = ResetFlags::VSYNC.bits(); // enable vsync
         init.platform_data = self.get_platform_data();
 
         if !bgfx_rs::static_lib::init(&init) {
             return Err(InitializationError::Bgfx);
         };
 
-        bgfx_rs::static_lib::set_debug(self.debug_flags.bits());
-        bgfx_rs::static_lib::set_view_clear(
-            0,
-            ClearFlags::COLOR.bits() | ClearFlags::DEPTH.bits(),
-            SetViewClearArgs {
-                rgba: 0x103030ff,
-                ..Default::default()
-            },
-        );
-
         Ok(())
     }
 
     /// Base event loop
-    pub fn run(&mut self) -> crate::error::Result<()> {
-        self.window.make_current();
-        self.window.set_key_polling(true);
-
-        while !self.window.should_close() {
-            // Swap front and back buffers
-            // self.window.swap_buffers();
-
-            // Poll for and process events
-            self.handle_events();
-
-            let size = self.window.get_framebuffer_size();
-
-            if self.size != size {
-                bgfx_rs::static_lib::reset(size.0 as _, size.1 as _, ResetArgs::default());
-                self.size = size;
-            }
-
-            bgfx_rs::static_lib::set_view_rect(0, 0, 0, size.0 as _, size.1 as _);
-            bgfx_rs::static_lib::touch(0);
-
-            bgfx_rs::static_lib::dbg_text_clear(DbgTextClearArgs::default());
-
-            bgfx_rs::static_lib::dbg_text(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
-            bgfx_rs::static_lib::dbg_text(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
-            bgfx_rs::static_lib::dbg_text(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
-            bgfx_rs::static_lib::dbg_text(
-                0,
-                4,
-                0x3f,
-                "Description: Initialization and debug text with bgfx-rs Rust API.",
-            );
-
-            bgfx_rs::static_lib::frame(false);
-        }
-
-        bgfx_rs::static_lib::shutdown();
-        Ok(())
+    pub fn run(
+        &mut self,
+        // executor: Box<dyn FnOnce(&mut Application) -> crate::error::Result<()>>,
+        executor: impl FnOnce(&mut Application) -> crate::error::Result<()>,
+    ) -> crate::error::Result<()> {
+        executor(self)
     }
 
-    fn handle_events(&mut self) {
+    pub fn handle_events(&mut self) {
         self.glfw.poll_events();
         glfw::flush_messages(&self.event_stream).for_each(|(_, event)| {
             println!("{:?}", event);
@@ -207,5 +162,11 @@ impl WindowHandle {
         }
 
         pd
+    }
+}
+
+impl AsMut<Application> for Application {
+    fn as_mut(&mut self) -> &mut Application {
+        self
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use bgfx_rs::static_lib::DebugFlags;
+use application::{Application, WindowMetadata};
+use bgfx_rs::static_lib::{ClearFlags, DbgTextClearArgs, DebugFlags, ResetArgs, SetViewClearArgs};
 use error::Result;
-use glfw::WindowMode;
-use window::{WindowHandle, WindowMetadata};
+use glfw::{Context, WindowMode};
 
+mod application;
 mod error;
-mod window;
+mod tile;
 
 const TITLE: &str = "Test window";
 const WIDTH: u32 = 1280;
@@ -13,8 +14,60 @@ const HEIGHT: u32 = 720;
 fn main() -> Result<()> {
     let metadata =
         WindowMetadata::new(TITLE, WIDTH, HEIGHT, WindowMode::Windowed, DebugFlags::TEXT);
-    let mut window = WindowHandle::try_new(metadata)?;
+    let mut application = Application::try_new(metadata)?;
 
-    window.init()?;
-    window.run()
+    application.init()?;
+    application.run(&executor)
+}
+
+fn executor(app: &mut Application) -> crate::error::Result<()> {
+    app.window.make_current();
+    app.window.set_key_polling(true);
+
+    bgfx_rs::static_lib::set_debug(app.debug_flags.bits());
+    bgfx_rs::static_lib::set_view_clear(
+        0,
+        ClearFlags::COLOR.bits() | ClearFlags::DEPTH.bits(),
+        SetViewClearArgs {
+            // rgba: 0x103030ff,
+            rgba: 0x443355FF,
+            ..Default::default()
+        },
+    );
+
+    while !app.window.should_close() {
+        // Swap front and back buffers
+        // self.window.swap_buffers();
+
+        // Poll for and process events
+        app.handle_events();
+
+        let size = app.window.get_framebuffer_size();
+        let size = (size.0 as u32, size.1 as u32);
+
+        if app.size != size {
+            bgfx_rs::static_lib::reset(size.0, size.1, ResetArgs::default());
+            app.size = size;
+        }
+
+        bgfx_rs::static_lib::set_view_rect(0, 0, 0, size.0 as u16, size.1 as u16);
+        bgfx_rs::static_lib::touch(0);
+
+        bgfx_rs::static_lib::dbg_text_clear(DbgTextClearArgs::default());
+
+        bgfx_rs::static_lib::dbg_text(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
+        bgfx_rs::static_lib::dbg_text(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
+        bgfx_rs::static_lib::dbg_text(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
+        bgfx_rs::static_lib::dbg_text(
+            0,
+            4,
+            0x3f,
+            "Description: Initialization and debug text with bgfx-rs Rust API.",
+        );
+
+        bgfx_rs::static_lib::frame(false);
+    }
+
+    bgfx_rs::static_lib::shutdown();
+    Ok(())
 }
